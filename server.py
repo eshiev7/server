b48eadb4bd5b57c272406feb18ef45b9653f17c3
eshiev7 <39335526+eshiev7@users.noreply.github.com>
2019-01-12 11:57:18 +0700

    Update server.py

--- server.py
+++ server.py
@@ -9,7 +9,7 @@ class ClientServerProtocol(asyncio.Protocol):
     def data_received(self, data):
         global info
         resp = data.decode()
-        data_uns = ""
+        data_answer = ""
         if not resp:
              return "error\nwrong command\n\n"
         data_temp = resp.replace("\n", "").split()
@@ -19,21 +19,21 @@ class ClientServerProtocol(asyncio.Protocol):
             else:
                 info = info.replace(resp.replace("put ", ""), "")
                 info  += resp.replace("put ", "")
-            data_uns = "ok\n\n"
+            data_answer = "ok\n\n"
         elif data_temp[0] == "get":
-            data_uns = "ok\n"
+            data_answer = "ok\n"
             key = data_temp[1]
             metrics = info.split("\n")
             if key == "*":
-                data_uns += info
+                data_answer += info
             else:
                 for metric in metrics:
                     if len(metric.split()) == 3 and metric.split()[0] == key:
-                        data_uns+=metric + "\n"
-            data_uns  += "\n"
+                        data_answer+=metric + "\n"
+            data_answer  += "\n"
         else:
-            data_uns = "error\nwrong command\n\n"
-        self.transport.write(data_uns.encode())
+            data_answer = "error\nwrong command\n\n"
+        self.transport.write(data_answer.encode())
 
 loop = asyncio.get_event_loop()
 coro = loop.create_server(ClientServerProtocol,"127.0.0.1", 8181)
